8639e9dc8e6f07fe4ed01f45ac97fb0f33531589
Mark Plomer <mark.plomer@boerse-go.de>
2015-03-26 13:32:59 +0100

    Expose error objects in flora module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     Api: require('./lib/api'),
+    errors: require('./lib/errors'),
     Master: require('./lib/master'),
     Request: require('./lib/request'),
     Server: require('./lib/server')
