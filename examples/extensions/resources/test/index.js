7d9091662397aaff5a535e5c373a32c473b57a34
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-11-21 17:00:00 +0100

    Improve documentation of Events and Extensions

--- examples/extensions/resources/test/index.js
+++ examples/extensions/resources/test/index.js
@@ -2,48 +2,39 @@
 
 module.exports = (api) => ({
     extensions: {
-        // "init" extension
+        // Extension: "init"
         // is called once upon startup, when all resources are initialized
         init: async function () {
             api.log.info('Extension: resource init');
         },
 
-        // "request" extension
-        request: async function (ev) {
-            var request = ev.request;
+        // Extension: "request"
+        request: async function ({ request, response }) {
             api.log.info('Extension: resource request');
         },
 
-        // "item" extension
+        // Extension: "item"
         // is called for every item that is handled by the resource-processor, also when the
         // resource is called as sub-resource from another resource.
-        item: function (ev) {
-            var request = ev.request;
-            var item = ev.item;
+        item: function ({ item, row, secondaryRows, request, getAttribute, getResult, buildItem }) {
             api.log.info('Extension: resource item');
             item.bar = 'baz';
         },
 
-        // "preExecute" extension
+        // Extension: "preExecute"
         // is called after the request-resolver has resolved the dataSourceTree.
-        preExecute: async function (ev) {
-            var request = ev.request;
-            var dataSourceTree = ev.dataSourceTree;
+        preExecute: async function ({ name, request, dataSourceTree, floraRequest }) {
             api.log.info('Extension: resource preExecute');
         },
 
-        // "postExecute" extension
+        // Extension: "postExecute"
         // is called after the request has been executed and before the response is being built
-        postExecute: async function (ev) {
-            var request = ev.request;
-            var rawResults = ev.rawResults;
+        postExecute: async function ({ name, request, floraRequest, rawResults }) {
             api.log.info('Extension: resource postExecute');
         },
 
-        // "response" extension
-        response: function (ev) {
-            var request = ev.request;
-            var response = ev.response;
+        // Extension: "response"
+        response: function ({ request, response }) {
             api.log.info('Extension: resource response');
         }
     },
--- examples/extensions/server.js
+++ examples/extensions/server.js
@@ -9,48 +9,37 @@ server.run();
 
 // http://localhost:3000/test/
 
-
-// Extension: "init"
+// Event: "init"
 // is called when Api is done initializing.
-
-server.api.on('init', async (ev) => {
+server.api.on('init', async () => {
     server.api.log.info('Extension: api init');
 });
 
-
-// Extension: "close"
+// Event: "close"
 // is called when Api is closing.
-
-server.api.on('close', async (ev) => {
+server.api.on('close', async () => {
     server.api.log.info('Extension: api close');
 });
 
-
-// Extension: "request"
+// Event: "request"
 // is called on each request, before the request is handled.
-
-server.api.on('request', async (ev) => {
-    const { request /* , response */ } = ev;
+server.api.on('request', async ({ request, response }) => {
     server.api.log.info('Extension: api request');
     request.limit = 1; // modify the "limit" parameter
     request.select = 'foo'; // modify the "select" parameter to always select (only) the "foo" attribute
     // we could throw an error if something goes wrong here
 });
 
-// Extension: "httpRequest"
+// Event: "httpRequest"
 // is called before any flora.Request instanciation and allows modifying HTTP headers
-
-server.api.on('httpRequest', (ev) => {
-    const { httpRequest, httpResponse } = ev;
+server.api.on('httpRequest', ({ httpRequest, httpResponse }) => {
     server.api.log.info('Extension: api httpRequest');
     httpResponse.setHeader('X-Hello', 'World');
 });
 
-// Extension: "response"
+// Event: "response"
 // is called after the request is handled and before the response is sent.
-
-server.api.on('response', async (ev) => {
-    const { response } = ev;
+server.api.on('response', async ({ request, response }) => {
     server.api.log.info('Extension: api response');
 
     // modify response: add "baz: 'foo'" property to the complete response
